e94ab92d88b17502c37a6ce197e2a5adde859a02
amit6388 <amitpoly2020@gmail.com>
2023-07-25 08:56:46 +0530

    last commit

--- multer/admin/joininstructor_upload.js
+++ multer/admin/joininstructor_upload.js
@@ -9,7 +9,7 @@ var storage=multer.diskStorage({
 });
 
 var fileFilter=(req,file,cb)=>{
-    if(file.mimetype==='image/jpeg' || file.mimetype==='image/jpg' || file.mimetype==='image/png'){
+    if(file.mimetype=='application/pdf' || file.mimetype=='application/vnd.openxmlformats-officedocument.wordprocessingml.document' || file.mimetype=='application/msword'){
         cb(null,true)
     }else{
         cb(null,false)
