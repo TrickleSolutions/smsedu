94f61bb21d6d3c3ab97cbeafb066c2b69054ad63
AbhishekTsFullStack <145094267+AbhishekTsFullStack@users.noreply.github.com>
2023-12-14 15:14:30 +0530

    Updated the new search api

--- models/students/EnrollCourse.js
+++ models/students/EnrollCourse.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
 const EnrollCourseSchema = new mongoose.Schema({
-     
-    student: {
-        type: String,
-       // required: true,
-      },
-      
-       
-      course: {
-        type: String,
-      //  required: true,
-      },
-      
-      create_at: {
-        type: String,
-      //  required: true,
-      }
-    
-    
-}); 
-module.exports = mongoose.model('Enrollcourses_tbl', EnrollCourseSchema)
- 
+  student: {
+    type: String,
+    // required: true,
+  },
+
+  course: {
+    type: String,
+    //  required: true,
+  },
+
+  create_at: {
+    type: String,
+    //  required: true,
+  },
+});
+module.exports = mongoose.model("Enrollcourses_tbl", EnrollCourseSchema);
